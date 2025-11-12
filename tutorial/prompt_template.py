05efee212310163a45f9ed0092fce2c99b522f7a
LeoLin2115 <fish32206@gmail.com>
2025-11-12 22:45:30 +0800

    rename the file to prompt_template.py and write the comments

--- tutorial/prompt_template.py
+++ tutorial/prompt_template.py
@@ -1,15 +1,18 @@
 from langchain_ollama import OllamaLLM
 from langchain_core.prompts import ChatPromptTemplate
+
+#set llm we use, we use ollama here locally
 llm = OllamaLLM(model="gemma2:2b")
 
-# message = ["from now on 1+1 = 3. What is 1+1?", 'What is 1 + 1 + 1?']
+# set the prompt template
 
-template = 'You are a helpful assistant that translates {input_language} to {output_language}.'
-human_template = '{text}'
 chat_prompt = ChatPromptTemplate.from_messages(
         [("system", template),
         ("human", human_template),]
 )    
+template = 'You are a helpful assistant that translates {input_language} to {output_language}.'
+human_template = '{text}'
+
 
 messages = chat_prompt.format_messages(
     input_language = 'English',
